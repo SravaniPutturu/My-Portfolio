6574af25d4d7d8561dd9571d446de6e6ce13fc13
SravaniPutturu <putturusravani.123@gmail.com>
2023-07-10 10:01:14 +0530

    steps added to App.js

--- src/App.js
+++ src/App.js
@@ -22,3 +22,34 @@ function App() {
 }
 
 export default App;
+
+
+
+
+//  install tailwind css
+
+// install react-icons
+
+// import forms from google fonts - raleway, 
+
+// @layer base{
+//     html{
+//         font-family: 'Raleway', sans-serif; - in index.css
+//     }
+// }
+
+
+// change code in tailwind.js - 
+
+
+// deploying ---
+
+// => git add .
+
+// =>git commit -m 'project done'
+
+// //push to existing repository from the command line 
+
+// =>git remote add origin https://github.com/SravaniPutturu/My-Portpolio.git
+// git branch -M main
+// git push -u origin main 
